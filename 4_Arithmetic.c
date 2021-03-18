ef6f7bcb54647b9c4534c42ccea3030d33a22594
riyag181 <2020a1r181@mietjammu.in>
2021-03-18 19:04:22 +0530

    simple calculator

--- 4_Arithmetic.c
+++ 4_Arithmetic.c
@@ -2,26 +2,26 @@
 int main(){
     char operator;
     double a, b;
-    printf("Enter an operator (+, -, *, /):");
-    scanf("%c", &operator);
-    printf("Enter two operands:");
-    scanf("%lf %lf",&a, &b);
+    printf("Enter an operator (+, -, *, /):");//printing  the operator to be entered by the user
+    scanf("%c", &operator);//scanning operators
+    printf("Enter two operands:");//printing the numbers 
+    scanf("%lf %lf",&a, &b);//scanning numbers
     switch(operator)
     {
         case '+':
-            printf("%.lf + %.lf = %.lf",a, b, a+b);
+            printf("%.lf + %.lf = %.lf",a, b, a+b);//addition funtion
             break;
 
         case '-':
-            printf("%.lf - %.lf = %.lf",a, b, a-b);
+            printf("%.lf - %.lf = %.lf",a, b, a-b);//subtraction function
             break;
 
         case '*':
-            printf("%.lf * %.lf = %.lf",a, b, a*b);
+            printf("%.lf * %.lf = %.lf",a, b, a*b);//mutliplication function
             break;
 
         case '/':
-            printf("%.lf / %.lf = %.lf",a, b, a/b);
+            printf("%.lf / %.lf = %.lf",a, b, a/b);//division function
             break;
     printf("Error encountered by the input values");
     }
