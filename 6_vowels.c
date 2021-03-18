5a0defc2acab536e55c20abd9e79d2129a008e4a
riyag181 <2020a1r181@mietjammu.in>
2021-03-19 00:26:24 +0530

    vowels

--- 6_vowels.c
+++ 6_vowels.c
@@ -3,14 +3,14 @@ int main() {
     char c;
     int lowercase_vowel, uppercase_vowel;
     printf("Enter an alphabet: ");
-    scanf("%c", &c);
+    scanf("%c", &c);//scanning input
     lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 
     uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
     if (lowercase_vowel || uppercase_vowel)
-        printf("%c is a vowel.", c);
+        printf("%c is a vowel.", c);//alphabet is a vowel
     else
-        printf("%c is a consonant.", c);
+        printf("%c is a consonant.", c);//alphabet is a consonant
     return 0;
 }
