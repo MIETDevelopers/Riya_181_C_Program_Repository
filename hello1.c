6441bcd32a21aef878be6c19d55e91ba0543fa14
riyag181 <2020a1r181@mietjammu.in>
2021-03-20 14:53:21 +0530

    message

--- hello1.c
+++ hello1.c
@@ -1,6 +1,6 @@
 //Program to print hello world
 #include<stdio.h>//pre processor directive to inculde input output functions
-int main() 
+int main()//main funtion 
 {
 	printf("Hello World");//Printing hello world
 	return 0;
