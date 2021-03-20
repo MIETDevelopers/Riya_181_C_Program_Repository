1bf379955a4031c5998b032f8523d60eb79c6969
riyag181 <2020a1r181@mietjammu.in>
2021-03-20 14:55:44 +0530

    sum of 2 no.s

--- sum_2.c
+++ sum_2.c
@@ -1,7 +1,8 @@
 //Program to calculate sum of two numbers.			
-#include<stdio.h>
-int main(){
-	int number1, number2, sum;
+#include<stdio.h>//header file
+int main()//main function
+{
+	int number1, number2, sum;//decalaration
 	scanf("%d %d", &number1, &number2); //Scanning values entered by user.
 	sum=number1+number2; //Processing the sum operation.
 	printf("Sum of 2 numbers= %d", sum); //Printing the output.
