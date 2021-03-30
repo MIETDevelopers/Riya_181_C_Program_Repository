6844ec96974c2f65c213092792956d7f4c870f46
riyag181 <2020a1r181@mietjammu.in>
2021-03-30 23:34:33 +0530

    addition of 2 2D array

---
+++ 2D_array25.c
@@ -0,0 +1,26 @@
+//Program to demonstrate addition of 2 2-D arrays.
+#include <stdio.h>
+int main()
+{
+	int a[2][2],b[2][2],c[2][2],i,j;
+	printf("\nENTER VALUES FOR MATRIX A:\n");
+	for(i=0;i<2;i++)
+		for(j=0;j<2;j++)
+			scanf("%d",&a[i][j]);
+	printf("\nENTER VALUES FOR MATRIX B:\n");
+	for(i=0;i<2;i++)
+		for(j=0;j<2;j++)
+			scanf("%d",&b[i][j]);
+	for(i=0;i<2;i++)
+		for(j=0;j<2;j++)
+			c[i][j]=a[i][j]+b[i][j];
+	printf("\nTHE VALUES OF MATRIX C ARE:\n");
+	for(i=0;i<2;i++)
+        {
+		for(j=0;j<2;j++)
+			printf("%5d",c[i][j]);
+		printf("\n");
+	}
+	return 0;
+
+}
