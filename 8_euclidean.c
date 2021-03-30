4131cae52ac1f72078f96284d856b369ae83aa49
riyag181 <2020a1r181@mietjammu.in>
2021-03-30 18:07:25 +0530

    do while loop

--- 8_euclidean.c
+++ 8_euclidean.c
@@ -12,7 +12,7 @@ int main()//main function
     printf("Enter point 2 (x2, y2)\n");  
     scanf("%f %f", &x2, &y2); //scanning input
   
-    distance = sqrt( (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) );// formula 
+    distance = sqrt( (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) );// formula 
   
     printf("Distance between (%0.2f, %0.2f) and (%0.2f, %0.2f) is %0.2f\n", x1, y1, x2, y2, distance);//output  
   
---
+++ hello1.c
@@ -0,0 +1,7 @@
+//Program to print hello world
+#include<stdio.h>//pre processor directive to inculde input output functions
+int main()//main funtion 
+{
+	printf("Hello World");//Printing hello world
+	return 0;
+}
