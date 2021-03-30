33556e92a4c9b3491a47ac38221eb577ddf64d5b
riyag181 <2020a1r181@mietjammu.in>
2021-03-31 01:00:44 +0530

    reading csv file

---
+++ read_csv35.c
@@ -0,0 +1,30 @@
+//Program to read csv file.
+#include <stdio.h> //Pre-Processor Directive to Include standard input output header files.
+#include <stdlib.h>//Pre-Processor Directive to Include standard library header files.
+#include <string.h>//re-Processor Directive to Include string header files. 
+
+const char* getfield(char* line, int num)
+{
+    const char* tok;
+    for (tok = strtok(line, ";");
+            tok && *tok;
+            tok = strtok(NULL, ";\n"))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
+int main() //Main function body starting
+{	
+	//Path of the .csv file.
+    FILE* stream = fopen("C:\\Desktop\\C_Programs\\Rajveer_122_C_Programs_Repository\\read_csv35.csv", "r"); 
+
+    char line[1024];
+    while (fgets(line, 1024, stream))
+    {
+        char* tmp = strdup(line);
+        printf("Field 3 would be %s\n", getfield(tmp, 3));
+        free(tmp);
+    }
+} //main function ends
