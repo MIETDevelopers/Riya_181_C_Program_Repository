3665f3349f1b32d9832b558c01dd9fb3d9478803
riyag181 <2020a1r181@mietjammu.in>
2021-03-31 00:06:04 +0530

    pointer variable initialization

---
+++ struct_implementation28.c
@@ -0,0 +1,28 @@
+//Program to Student structure implimentation which should contain, name, marks in different subjects, attendance percentage, Roll Number as members.
+
+#include <stdio.h> //Pre processor directive to include standard input output functions header file
+struct student { //structure body
+    char name[50];
+    int rollno;
+    float marks;
+} s;
+
+int main() { //main function body starting
+
+    printf("Enter information:\n");
+    printf("Enter name: ");
+    fgets(s.name, sizeof(s.name), stdin);
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.rollno);
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    printf("Displaying Information:\n");
+    printf("Name: ");
+    scanf("%s", s.name);
+    scanf("Roll number: %d\n", s.rollno);
+    scanf("Marks: %.1f\n", s.marks);
+
+  return 0;
+}
